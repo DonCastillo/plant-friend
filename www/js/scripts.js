33b2ab13fc42ebe033cedd7c1a35b75e3d145be1
Don <don.qcastillo@yahoo.com>
2022-10-19 11:47:30 -0600

    updated js

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -5,6 +5,31 @@ var app = new Framework7({
 });
 var mainView = app.views.create('.view-main')
 
+var waterLevel = 20;
+var noWater = 0;
+var drowning = 40;
+
+var nearDeath = false;
+
+function dryout() {
+  waterLevel--;
+  console.log(waterLevel);
+  checkHealth();
+  var watertimer = setTimeout(dryout, 500);
+}
+
+dryout();
+
+function checkHealth() {
+  if(waterLevel <= noWater) {
+    nearDeath = true;
+    console.log("Help!");
+    $('#plant path').css('fill', '#d2691e');
+  }
+}
+
+
+
 // by default:
 // - dry out over time
 // - deplete in nutrients over time
